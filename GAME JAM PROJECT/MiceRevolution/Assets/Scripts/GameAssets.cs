69e3226ca48699c508801dc4d52d1baa9193998c
Joshua Tipton <72083305+JoshuaTipton@users.noreply.github.com>
2020-12-07 21:25:49 +0000

    uploaded game jam

---
+++ GAME JAM PROJECT/MiceRevolution/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets i;
+
+    private void Awake()
+    {
+        i = this;
+    }
+    
+    public Sprite snakeHeadSprite;
+    public Sprite snakeBodySprite;
+    public Sprite foodSprite;
+}
---
+++ GAME JAM PROJECT/MiceRevolution/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    private static GameHandler instance;
+
+    private static int score;
+
+    [SerializeField] private Snake snake;
+    private LevelGrid levelGrid;
+
+    private void Awake()
+    {
+        instance = this;
+        InitializeStatic();
+    }
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("GameHanler.Start");
+
+        levelGrid = new LevelGrid(20, 20);
+
+        snake.Setup(levelGrid);
+        levelGrid.Setup(snake);
+
+        //CMDebug.ButtonUI(Vector2.zero, "Reload Scene", () => {
+        //    Loader.Load(Loader.Scene.GameScene);
+        //});
+    }
+
+    private static void InitializeStatic()
+    {
+        score = 0;
+    }
+
+    public static int GetScore()
+    {
+        return score;
+    }
+    public static void AddScore()
+    {
+        score += 1;
+    }
+    public static void SnakeDied()
+    {
+        GameOverWindow.ShowStatic();
+    }
+}
